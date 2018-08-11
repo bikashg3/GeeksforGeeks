33c0c5f2abbfbba417457cb56998e9cd0ae76703
Bikash Gupta <bikashg3@users.noreply.github.com>
2018-08-11 17:44:09 +0530

    Create kadane.py

---
+++ Arrays/1.kadaneAlgo/kadane.py
@@ -0,0 +1,14 @@
+def kadane(arr,n):
+    gmax=arr[0]
+    curmax=arr[0]
+    for i in range(1,n):
+        curmax=max(curmax,arr[i]+curmax)
+        gmax=max(curmax,gmax)
+    return gmax
+
+t=int(input())
+while t>0:
+    n=int(input())
+    arr=list(map(int,input().split()))
+    print(kadane(arr,n))
+    t=t-1
